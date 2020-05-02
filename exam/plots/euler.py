85734efeb8934109d79c0879e6486bb7e988feec
Patka Zsolt-Andras <andras.patka@gmail.com>
2020-05-02 22:56:59 +0300

    Added doc structure for exam assignments, added some plots

---
+++ exam/plots/euler.py
@@ -0,0 +1,16 @@
+from scipy import signal
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+t = np.linspace(0, 2 * np.pi, 1000)
+
+def f(t): return np.exp(- np.complex(0, 1) * t)
+def c(t): return np.cos(t) + np.complex(0, 1) * np.sin(t)
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(f(t).real, f(t).imag, "b")
+plt.subplot(212)
+plt.plot(c(t).real, c(t).imag, "b")
+plt.show()
